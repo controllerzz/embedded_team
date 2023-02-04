2d25513b6b032d53de49f548ad67cf6acf712d6d
Mike <34655997+controllerzz@users.noreply.github.com>
2023-02-04 21:54:05 +0300

    Update stm32_secure.c

--- stm32_secure.c
+++ stm32_secure.c
@@ -1,15 +1,16 @@
-  FLASH_OBProgramInitTypeDef obConfig;
-  HAL_FLASHEx_OBGetConfig(&obConfig);
+FLASH_OBProgramInitTypeDef obConfig;
+HAL_FLASHEx_OBGetConfig(&obConfig);
 
-  if (obConfig.RDPLevel == OB_RDP_LEVEL_0)
-  {
-    obConfig.RDPLevel = OB_RDP_LEVEL_1;
-    HAL_FLASH_Unlock();
-    HAL_FLASH_OB_Unlock();
-    HAL_FLASHEx_OBProgram(&obConfig);
-    HAL_FLASH_OB_Launch();
-  }
-  else
-  {
-	  __HAL_AFIO_REMAP_SWJ_DISABLE();
-  }
+// если защита от чтения не включена,
+// тогда включаем ее
+if (obConfig.RDPLevel == OB_RDP_LEVEL_0)
+{
+  obConfig.RDPLevel = OB_RDP_LEVEL_1;
+  HAL_FLASH_Unlock();
+  HAL_FLASH_OB_Unlock();
+  HAL_FLASHEx_OBProgram(&obConfig);
+  HAL_FLASH_OB_Launch();
+}
+
+// отключаем SWD и JTAG
+__HAL_AFIO_REMAP_SWJ_DISABLE();
